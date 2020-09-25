d3c74f97f81646118712491f41f009e7a984472e
Ádám Tilly <tilly.adam@gmail.com>
2020-09-25 10:29:22 +0200

    Harcos get;

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -15,21 +15,22 @@ namespace HarcosProjekt
         private int alapEletero;
         private int alapSebzes;
 
-        public Harcos(string nev, int szint, int tapasztalat, int eletero, int alapEletero, int alapSebzes)
+        public Harcos(string nev, int statuszSablon)
         {
-            this.nev = nev;
-            this.szint = szint;
-            this.tapasztalat = tapasztalat;
-            this.eletero = eletero;
-            this.alapEletero = alapEletero;
-            this.alapSebzes = alapSebzes;
+            this.nev = nev;            
+
         }
 
+        
         public string Nev { get => nev; set => nev = value; }
         public int Szint { get => szint; set => szint = value; }
         public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
         public int Eletero { get => eletero; set => eletero = value; }
         public int AlapEletero { get => alapEletero; set => alapEletero = value; }
-        public int AlapSebzes { get => alapSebzes; set => alapSebzes = value; }
+        public int AlapSebzes { get => alapSebzes; set => alapSebzes = value; }   
+        public int Sebzes { get => alapSebzes + szint; }
+        public int SzintLepeshez { get => 10 + szint * 5; }
+        public int MaxEletero { get => alapEletero + szint * 3; }
+
     }
 }
