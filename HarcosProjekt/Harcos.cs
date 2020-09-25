89f457304ae9820715808dbe30e2a4cca5d9d13e
Ádám Tilly <tilly.adam@gmail.com>
2020-09-25 10:09:28 +0200

    Harcos class elemeinek deklaralasa

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -8,5 +8,11 @@ namespace HarcosProjekt
 {
     class Harcos
     {
+        private string nev;
+        private int szint;
+        private int tapasztalat;
+        private int eletero;
+        private int alapEletero;
+        private int alapSebzes;
     }
 }
