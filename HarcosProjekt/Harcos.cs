f4fd5274b92fc52fa65658c23f10fd52b540624b
Ádám Tilly <tilly.adam@gmail.com>
2020-09-25 10:11:42 +0200

    Harcos class konstruktor

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -14,5 +14,15 @@ namespace HarcosProjekt
         private int eletero;
         private int alapEletero;
         private int alapSebzes;
+
+        public Harcos(string nev, int szint, int tapasztalat, int eletero, int alapEletero, int alapSebzes)
+        {
+            this.nev = nev;
+            this.szint = szint;
+            this.tapasztalat = tapasztalat;
+            this.eletero = eletero;
+            this.alapEletero = alapEletero;
+            this.alapSebzes = alapSebzes;
+        }
     }
 }
